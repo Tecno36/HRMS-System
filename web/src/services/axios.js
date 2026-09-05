5cddc276cfbc5c0f1e281a1b005f2577c6cb75dc
Tecno36 <tmetkari143@gmail.com>
2026-09-05 22:36:18 +0530

    fix: force git to update file name case

--- web/src/services/axios.js
+++ web/src/services/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const baseURL = isLocalhost 
+    ? 'http://localhost:5000/api' 
+    : 'https://hrms-system-yri7.onrender.com/api';
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json'
     }
