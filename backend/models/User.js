448e35b8e9900004d571e6fd643596178f15ff5c
Tecno36 <tmetkari143@gmail.com>
2026-09-10 17:42:45 +0530

    changes in push notification

--- backend/models/User.js
+++ backend/models/User.js
@@ -50,6 +50,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: null
     },
+    fcmToken: { 
+        type: String, 
+        default: null 
+    },
     isBiometricEnabled: {
         type: Boolean,
         default: false
